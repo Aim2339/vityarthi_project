d5e8d95e88685c2be2383237a553310142dd7bef
Aim <aaravsciju@gmail.com>
2025-11-20 00:33:36 +0530

    Add password-login system

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ from reports import report_card
 from visualize import plot_marks
 from roster import class_roster
 
+print("SYSTEM LOCKED")
+password = input("Enter Password: ")
 
-print("Teacher's Gradebook: ")
-print("1. View Class Roster")
-print("2. View Student Report Card")
-print("3. Visualize Student Marks")
-choice = input("Please choose an option (1-3): ")
+if password == "123":
+    print("Access Granted.\n")
+    print("Teacher's Gradebook: ")
+    print("1. View Class Roster")
+    print("2. View Student Report Card")
+    print("3. Visualize Student Marks")
 
-if choice == '1':
-    class_roster()
+    choice = input("Please choose an option (1-3): ")
 
-elif choice == '2':
-    name = input("\nEnter student name: ")
-    report_card(name)
+    if choice == '1':
+        class_roster()
 
-elif choice == '3':
-    name = input("\nEnter student name: ")
-    plot_marks(name)
+    elif choice == '2':
+        name = input("\nEnter student name: ")
+        report_card(name)
+
+    elif choice == '3':
+        name = input("\nEnter student name: ")
+        plot_marks(name)
+    else:
+        print("Please enter a number from 1 to 3.")
+
 else:
-    print("Please enter a number from 1 to 3.")
+    print("Access Denied. Incorrect Password.")
